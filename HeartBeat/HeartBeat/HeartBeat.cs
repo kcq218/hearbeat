5f79814ddfb0347f81c68219b3632d408f6e82b4
kcq218 <khangquach218@gmail.com>
2025-01-25 16:39:14 -0800

    add logging

--- HeartBeat/HeartBeat/HeartBeat.cs
+++ HeartBeat/HeartBeat/HeartBeat.cs
@@ -15,18 +15,24 @@ namespace HeartBeat
         [Function("HearBeat")]
         public void Run([TimerTrigger("*/30 * * * *")] TimerInfo myTimer)
         {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-            if (myTimer.ScheduleStatus is not null)
+            try
             {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+                _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+                if (myTimer.ScheduleStatus is not null)
+                {
+                    _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+                }
+
+                var context = new DbAll01ProdUswest001Context();
+
+                var read = context.UrlMappings.First();
+                _logger.LogInformation($"result of read: Id {read.Id}");
             }
-
-            var context = new DbAll01ProdUswest001Context();
-
-            var read = context.UrlMappings.First();
-            _logger.LogInformation($"result of read: Id {read.Id}");
-
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Error in HeartBeat");
+            }
         }
     }
 }
